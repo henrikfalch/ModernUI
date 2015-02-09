94448e8c7f042651b137fc389551540e08168042
Superlangbein <h.hesland@gmail.com>
2015-02-09 23:10:03 +0100

    away with some of the crap code

--- app/src/main/java/android/course/modernui/FullscreenActivity.java
+++ app/src/main/java/android/course/modernui/FullscreenActivity.java
@@ -1,7 +1,5 @@
 package android.course.modernui;
 
-import android.course.modernui.util.SystemUiHider;
-
 import android.app.Activity;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -15,42 +13,8 @@ import java.util.List;
 import static android.graphics.Color.rgb;
 
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- *
- * @see SystemUiHider
- */
+
 public class FullscreenActivity extends Activity {
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
-
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-    /**
-     * If set, will toggle the system UI visibility upon interaction. Otherwise,
-     * will show the system UI visibility upon interaction.
-     */
-    private static final boolean TOGGLE_ON_CLICK = true;
-
-    /**
-     * The flags to pass to {@link SystemUiHider#getInstance}.
-     */
-    private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
-
-    /**
-     * The instance of the {@link SystemUiHider} for this activity.
-     */
-    private SystemUiHider mSystemUiHider;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
